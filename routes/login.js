59926575069f828365e152368a4ff0f05ab6af99
avikant wadhwa <avikant16@hotmail.com>
2020-01-04 17:47:01 +0530

    create_route_dir

---
+++ routes/login.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const router=express.Router()
+const {fire,firebase,fireadmin}=require('../config/firebase.js')
+
+
+router.post("/",async( req,res) => {
+
+  const {email,password}=req.body;
+
+  try
+{
+  //await fire.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+
+  // the user signs in with email and password.
+  const user=await fire.auth().signInWithEmailAndPassword(email,password)
+
+   
+    if(!user.user.emailVerified)
+      return res.status(400).send("Email not verified")
+
+      const token=await user.user.getIdToken()
+
+      return res.json(token)
+        //console.log(user.user.emailVerified)
+
+}
+
+catch(err)
+{
+  return res.status(400).send(err.message)
+}
+ 
+
+
+})
+
+module.exports=router
---
+++ routes/register.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const router=express.Router()
+const {fire,firebase}=require('../config/firebase.js')
+
+
+router.post("/", async( req, res) => {
+
+    const {email,password}=req.body;
+ 
+    try
+    {
+      await fire.auth().createUserWithEmailAndPassword(email,password)
+ 
+      var user= fire.auth().currentUser
+      
+      await user.sendEmailVerification();
+ 
+      console.log(user.emailVerified)
+ 
+      
+      return res.status(200).send("Email link sent to verify email")
+   
+   
+   }
+   catch(err)
+   {
+     
+     return res.status(400).send(err.message)
+   }
+   
+   
+   
+ })
+
+ module.exports=router
