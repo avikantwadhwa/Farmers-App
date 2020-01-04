4269490bf2cbb4a7a9e1ea231990c482af28a854
avikant wadhwa <avikant16@hotmail.com>
2020-01-04 20:26:42 +0530

    auth_req

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router()
 const {fire,firebase,fireadmin}=require('../config/firebase.js')
-
+//const auth=require('../middleware/auth.js')
 
 router.post("/",async( req,res) => {
 
@@ -19,7 +19,7 @@ router.post("/",async( req,res) => {
       return res.status(400).send("Email not verified")
 
       const token=await user.user.getIdToken()
-
+      
       return res.json(token)
         //console.log(user.user.emailVerified)
 
