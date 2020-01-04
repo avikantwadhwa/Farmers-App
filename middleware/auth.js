c59bf4314b32d44cb44688f27d145bebc2faa856
avikant wadhwa <avikant16@hotmail.com>
2020-01-04 20:27:03 +0530

    auth_middleware

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const {fireadmin}=require('../config/firebase.js')
+
+module.exports=async function (req,res,next){
+
+   //console.log(req)
+   const token = req.header("x-auth-token");
+   
+   if(!token)
+    return res.status(401).json({ msg: "authorizationn denied" })
+
+   try
+   {
+   
+    const user= await fireadmin.auth().verifyIdToken(token)
+    
+    }
+   catch(err)
+   {
+       //console.log(err)
+       return res.status(401).json({"message":"Invalid token"})
+   }
+  
+
+  
+}
