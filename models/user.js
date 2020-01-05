9ebc352965c06da08200f157e95914c761a724be
yatharth17 <yatharthkatyal4@gmail.com>
2020-01-05 14:01:42 +0530

    added schema

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+});
+module.exports = User = mongoose.model("user", UserSchema);
