7addb4ad571c3856810b1756ead1bd71067a1f77
Mohak Chugh <me.mohakchugh@gmail.com>
2020-01-06 01:35:02 +0530

    Cors added

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express=require('express')
-var bodyParser=require('body-parser')
-const connectDB=require('./db/db')
+const express = require('express')
+const bodyParser = require('body-parser')
+const connectDB = require('./db/db')
+const cors = require('cors')
+var registeruser = require('./routes/register')
+var loginuser = require('./routes/login')
 
-var registeruser=require('./routes/register')
-var loginuser=require('./routes/login')
-
-const app=express();
+const app = express();
 connectDB();
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors())
+
+app.use("/registeruser", registeruser)
+app.use("/loginuser", loginuser)
 
 
-app.use("/registeruser",registeruser)
-app.use("/loginuser",loginuser)
+const port = process.env.PORT || 5000
 
 
-const port=process.env.PORT || 5000
-
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server Started on ${port}`)
 })
 
