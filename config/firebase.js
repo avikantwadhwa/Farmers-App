d2686fa0511f116060b4e27b40db23028cd1f0c5
avikant wadhwa <avikant16@hotmail.com>
2020-01-06 09:28:07 +0530

    reset_file

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,17 @@
 const firebase = require("firebase/app");
-const firebase_admin = require('firebase-admin')
+const firebase_admin=require('firebase-admin')
+const config=require('config')
 require("firebase/auth");
 global['XMLHttpRequest'] = require('xmlhttprequest').XMLHttpRequest;
-const { firebaseConfig } = require('./config')
+const firebaseConfig=config.get("firebaseConfig")
 
 const fire = firebase.initializeApp(firebaseConfig)
 
 
-const fireadmin = firebase_admin.initializeApp(firebaseConfig)
+const fireadmin=firebase_admin.initializeApp(firebaseConfig)
+  
 
-
-module.exports = { fire, firebase, fireadmin }
+module.exports={fire,firebase,fireadmin}
 
 
 
