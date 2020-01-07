a5c56f98b5fa2edde0d6943f62d49781bb1353aa
avikant wadhwa <avikant16@hotmail.com>
2020-01-07 21:00:41 +0530

    change_error_response

--- routes/login.js
+++ routes/login.js
@@ -5,19 +5,26 @@ const auth = require('../middleware/auth.js')
 
 router.post("/", async (req, res) => {
   let { email, password } = req.body;
+
   try {
-    //await fire.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+    
     let user = await fire.auth().signInWithEmailAndPassword(email, password)
+   
     if (!user.user.emailVerified)
       return res.send("Email not verified")
+
     const token = await user.user.getIdToken()
+    
     return res.send(token)
+  
   }
+  
   catch (err) {
-    return res.send(err.message)
+    
+    
+    return res.json(err.message)
   }
+
 })
-// router.post("/checking",auth,(req,res)=>{
-//   console.log("ok")
-// })
+
 module.exports = router
