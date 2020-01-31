89643d22eb615d18a9dcfaa9491af3beaa2718df
avikant wadhwa <avikant16@hotmail.com>
2020-01-31 18:35:43 +0530

    add_heroku

---
+++ routes/mail.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const request=require('request')
+const config=require('config')
+const nodemailer=require('nodemailer')
+
+const router = express.Router()
+
+  
+
+router.post("/", async (req, res) => {
+  
+
+  let {subject,text}=req.body;
+
+
+    try 
+  {
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      auth: {
+          user: '',
+          pass: ''
+      }
+  });
+  
+    const message = {
+        from: ' ', // Sender address
+        to: ' ',         // List of recipients
+        subject: subject, // Subject line
+        text: text // Plain text body
+    }
+
+    transporter.sendMail(message, function(err, info) {
+      if (err) {
+        return res.json(err)
+      } else {
+        return res.json(info)
+      }
+  });
+       
+  }
+  
+  catch (err) 
+  {
+    return res.json(err.message)
+    
+  }
+
+})
+
+module.exports = router
