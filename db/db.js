30784ba17878ee66b81a89e98d869babe0b3c9b3
aviwadh <32789515+avikantwadhwa@users.noreply.github.com>
2020-12-03 13:33:17 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("config");
-//const db = config.get("mongoURI");
 
-const db=config.get("mongoURI")
+const db = config.get("mongoURI")
 
 const connectDB = async () => {
   try {
