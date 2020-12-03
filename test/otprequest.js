e9a6a8077bd7083b11c2fae5f68bef4288b83462
aviwadh <32789515+avikantwadhwa@users.noreply.github.com>
2020-12-03 13:36:53 +0530

    Update otprequest.js

--- test/otprequest.js
+++ test/otprequest.js
@@ -6,7 +6,7 @@ var options = { method: 'POST',
    { 'postman-token': '8299572e-c8a5-52dd-667b-b21a52f1a352',
      'cache-control': 'no-cache',
      'content-type': 'application/x-www-form-urlencoded' },
-  form: { phone_number: '9811204579' } };
+  form: { phone_number: '' } };
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
