c6bc3983b276388e7ca7fbc92ad880986032bc3f
aviwadh <32789515+avikantwadhwa@users.noreply.github.com>
2020-12-03 13:30:16 +0530

    Update verifyotp.js

--- controllers/verifyotp.js
+++ controllers/verifyotp.js
@@ -6,7 +6,7 @@ const verifyotp = async (req, res) => {
     let { otp, session_id } = req.body;
     try {
         var url = `https://2factor.in/API/V1/${api_key}/SMS/VERIFY/${session_id}/${otp}`
-        request.get(url, function (err, response, body) {
+        request.get(url, function (err, response,body) {
             body = JSON.parse(body)
             if (err)
                 return res.json(err)
